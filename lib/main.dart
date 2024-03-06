cfee5204bf51faa8c6c67d56b58a3570e687cb3d
kritsanapong56 <kritsanapong115599@gmail.com>
2024-03-06 21:06:55 +0700

    ooo

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
+import 'dart:io';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/test.dart';
+import 'package:flutter_application_1/listmedicine.dart';
 import 'package:flutter_application_1/screen/homescreen.dart';
+import 'package:flutter_application_1/ui/Addtime.dart';
 
 
 void main() async {
-
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid? await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: 'AIzaSyCgI9yGbpdcU0IZaTZI2Mbqc6x3m_5PBEU',
+       appId: '1:56220203122:android:f08592288dd8f8f887aaeb', 
+       messagingSenderId: '56220203122', 
+       projectId: 'flutter-project-b8a70')
+  )
+  : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
@@ -12,9 +25,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyHomePage(),
+      home: listmedicine(),
     );
   }
 }
